3af4f1043f763fde2a32775bedebcfd5e4bdd86a
Mitali-Pandey <mailtomitalipandey@gmail.com>
2025-09-29 17:39:05 +0530

    Add files via upload

---
+++ dateCode.js
@@ -0,0 +1,13 @@
+var d = new Date();
+console.log(d)
+
+let date = new Date();
+
+console.log(date.getFullYear()); // Get full year (e.g., 2025)
+console.log(date.getMonth() + 1);    // Get month (0-11, January is 0)
+console.log(date.getDate());     // Get day of the month (1-31)
+console.log(date.getDay());      // Get day of the week (0-6, Sunday is 0)
+console.log(date.getHours());    // Get hours (0-23)
+console.log(date.getMinutes());  // Get minutes (0-59)
+console.log(date.getSeconds());  // Get seconds (0-59)
+console.log(date.getMilliseconds()); // Get milliseconds (0-999)
