3cfbf19dbc58ab8b5ed3b994b2729b4d8c512106
Mitali-Pandey <mailtomitalipandey@gmail.com>
2025-09-29 17:36:45 +0530

    Add files via upload

---
+++ create.js
@@ -0,0 +1,19 @@
+//1. Method 1
+var fruits = [ "apple", "orange", "mango" ];
+console.log(fruits)
+
+//2. Method 2 using keyword
+var arrayname=new Array();
+var i;  
+var emp = new Array();  
+emp[0] = "Arun";  
+emp[1] = "Deepak";  
+emp[2] = "Vijay";  
+console.log(emp) 
+
+//3. Method 3 constrcutor
+var emp2=new Array("Sandeep","Hardeep","Mandeep");  
+for (i=0;i<emp2.length;i++){  
+    console.log(emp2[i]); 
+}  
+
